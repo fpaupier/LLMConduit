a221e7d4b5dd4a89cae351ce4a171d31cb3aa4b7
cyrilou242 <cdecatheu@hey.com>
2023-08-17 22:27:02 +0200

    i was there

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from dotenv import load_dotenv
-from spellbook import template
+from spellbook import TEMPLATE
 
 import openai
 
@@ -51,7 +51,9 @@ def parse_llm_output(llm_output: str):
     try:
         json_output = json.loads(llm_output)
         if "intent" in json_output.keys():
-            print(json_output.intent)
+            process_intent()
+        elif "direct_response" in json_output.keys():
+            print(json_output["direct_response"])
     except ValueError as e:
         pass  # invalid json
     else:
@@ -89,3 +91,37 @@ INTENT_TO_API_MAP = {
 
 if __name__ == "__main__":
     main()
+
+# CONTEXT PROMPT
+# You provide a text interface for an API. There are 2 different intents: search a dataset (intent=SEARCH) and
+# get information for a dataset (intent=GET_DATASET)
+# I will provide you with user requests in this format:
+# USER_REQUEST: <the user request text>
+# Then you will read the USER_REQUEST and answer in json format. See examples below.
+#
+# The api provides a list of datasets. It is possible to search through datasets and get information about a dataset.
+# You have to detect between 2 different intents:
+# SEARCH: when the user wants to search available datasets for a given topic
+# GET_DATASET: when the user wants to get information about a specific topic
+#
+# For SEARCH, you will have to understand what would be a good search query based on the user request.
+# For instance: if the input is
+# USER_REQUEST: trouve les datasets au sujet de vélos
+# You should output in json format:
+# {"intent":SEARCH, "query": <velo>}
+# If you detect the intent is SEARCH, but don't have the full context and need to pursue the conversation, for instance
+# to get a search query, you should tell me you want to answer to the user directly by returning the following:
+# {"direct_response": "<the response you want to give to the user to make progress>"}
+# NOTE: the USER_REQUEST will be in French. The dataset names may be in French too.
+#
+# For GET_DATASET, you will have to understand what is the dataset you need to get. For instance, this is
+# not implemented, so you can just answer.
+# {"direct_response": "Getting a dataset is not implemented yet."}
+#
+# If you think the intent is neither GET_DATASET neither SEARCH, you should re-explain to the user
+# the goal of this service and continue the conversation. You can tell me what you want to answer to
+# the user by returning json format:
+# {"direct_response": "<your answer to the user>"}
+
+
+# TODO quand la demande est hors-contexte, utiliser un message hardcodé
